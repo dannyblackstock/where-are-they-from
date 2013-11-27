cce01364e77297214e2eea39ed096125bde48985
Danny Blackstock <dannyblackstock@gmail.com>
2013-11-26 22:49:36 -0800

    initial commit

---
+++ Artist Locations.py
@@ -0,0 +1,31 @@
+# to get user path on all operating systems
+from os.path import expanduser
+
+# path to user iTunes Library XML
+itunesLibraryPath = expanduser("~/Music/iTunes/iTunes Music Library.xml")
+
+artistList = []
+
+with open(itunesLibraryPath) as search:
+    
+    for line in search:
+        
+        if "<key>Artist</key>" in line:
+            artistName = line
+
+            #clean the string
+            artistName = artistName.replace("<key>Artist</key><string>", "")
+            artistName = artistName.replace("</string>", "")
+            artistName = artistName.replace("&#38;", "&")
+
+            # strip weird characters from beginning and end of line
+            artistName = artistName.strip() 
+
+            # add artist to the list
+            if artistName not in artistList:
+                artistList.append(artistName)
+
+artistList.sort()
+print(artistList)
+
+# to do origin and birthplace wikipedia
