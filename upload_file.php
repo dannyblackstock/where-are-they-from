19abc7541979208542a20e3a9fca1e011e390121
Danny Blackstock <dannyblackstock@gmail.com>
2014-06-27 01:24:22 -0700

    Hooked up to database. A lot more string cleaning and stuff.

--- upload_file.php
+++ upload_file.php
@@ -3,24 +3,26 @@ namespace CFPropertyList;
 require_once("CFPropertyList/CFPropertyList.php");
 require_once("sparqllib.php");
 
+// just in case...
+error_reporting( E_ALL );
+ini_set( "display_errors", "on" );
+
+// database information
 $dbhost = "localhost";
 $dbuser = "dblackst";
 $dbpass = "dblackst";
 $dbname = "where_are_they_from";
 
-// create new connection
-// @$db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-//
-// // Test if connection succeeded
-// // if connection failed, skip the rest of PHP code, and print an error
-// if ($db->connect_error)  {
-//     die('Connect Error: ' . $db->connect_error);
-// }
+// Connect to database.
+// Test if connection succeeded
+// if connection failed, skip the rest of PHP code, and print an error
+// use backslash to acces global namespace
+$mysqli = new \mysqli($dbhost, $dbuser, $dbpass, $dbname);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+}
 
-// just in case...
-error_reporting( E_ALL );
-ini_set( "display_errors", "on" );
-
+// only allow xml files to be uploaded
 $allowedExts = array("xml");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
@@ -31,15 +33,17 @@ if ((($_FILES["file"]["type"] == "application/xml")
 && ($_FILES["file"]["size"] < 20000000)
 && in_array($extension, $allowedExts)) {
 
+  // Print an error message if the file didn't work
   if ($_FILES["file"]["error"] > 0) {
     echo "Error: " . $_FILES["file"]["error"] . "<br>";
   }
 
   else {
+    // Print the uploaded file info
     echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Stored in: " . $_FILES["file"]["tmp_name"];
+    echo "Stored in: " . $_FILES["file"]["tmp_name"]; // temporary location
     echo "<br>";
 
     /*
@@ -57,7 +61,9 @@ if ((($_FILES["file"]["type"] == "application/xml")
 
     // Create an emp[ty list of artists.
     $artistList = [];
-    $db = sparql_connect("http://dbpedia.org/sparql");
+
+    // set sparql connection to dbpedia
+    $dbpedia = sparql_connect("http://dbpedia.org/sparql");
 
     // Loop through all tracks in the library XML file.
     foreach ($library["Tracks"] as $track) {
@@ -65,8 +71,13 @@ if ((($_FILES["file"]["type"] == "application/xml")
       // Check if their is an artist for the track.
       if (array_key_exists("Artist", $track)) {
         $artist = $track["Artist"];
+        // remove all extra spaces
+        $artist = trim($artist);
+        $artist = preg_replace('!\s+!', ' ', $artist);
+        //utf-8 decode
+        $artist = utf8_decode($artist);
 
-        // Check if the artist is in the list of artists already, if they're not, apend them to it.
+        // Now check if the artist is in the list of artists already, if they're not, append them to it.
         if (!in_array($artist, $artistList)) {
           array_push($artistList, $artist);
         }
@@ -74,68 +85,94 @@ if ((($_FILES["file"]["type"] == "application/xml")
       // echo $artist . "<br>";
     }
 
-    //sort alphabetically ("naturally")
+    //sort the list of artists alphabetically ("naturally")
     natsort($artistList);
 
     // var_dump( $plist->toArray() );
+
+    // Loop through the sorted artist list
     foreach($artistList as $artist) {
       echo "<h1>" . $artist . "</h1><br>";
 
-      $underscoreArtist = str_replace(" ", "_", $artist);
-      $lowerCaseAnd =  str_replace("And", "and", $underscoreArtist);
-      // try a few different combinations of names
-      $artistNames = [$underscoreArtist, $underscoreArtist . "_(band)", $underscoreArtist . "_(musician)", $lowerCaseAnd];
+      $origin = "";
+      $res = $mysqli->query("SELECT * FROM `artist_locations` WHERE `artist` = ".$artist);
 
-      // look for the birth place or origin of the artist on dbpedia
-      for ($i = 0; $i <= (count($artistNames)-1); $i++) {
-        $query = "
-        PREFIX property: <http://dbpedia.org/property/>
-        PREFIX dbpedia: <http://dbpedia.org/resource/>
-        PREFIX ontology: <http://dbpedia.org/ontology/>
+      // if the artist wasn't found in the database, check dbpedia for their origin and add them to the database.
+      if(empty($res)) {
+        echo $artist . " was not in the database! Adding now...<br>";
 
-        SELECT DISTINCT ?birth_place ?band_origin
-        WHERE {
-          OPTIONAL {<http://dbpedia.org/resource/".$artistNames[$i]."> ontology:birthPlace ?birth_place} .
-          OPTIONAL {<http://dbpedia.org/resource/".$artistNames[$i]."> property:origin ?band_origin} .
-        }
-        LIMIT 5";
+        $underscoreArtist = str_replace(" ", "_", $artist);
+        $lowerCaseAnd =  str_replace("And", "and", $underscoreArtist);
+        $upperCaseWords = ucwords(strtolower($underscoreArtist));
+        
+        // try a few different combinations of names
+        $artistNames = [$underscoreArtist, $upperCaseWords, $underscoreArtist . "_(band)", $upperCaseWords . "_(band)", $underscoreArtist . "_(musician)", $lowerCaseAnd];
 
-        // print which artist names have been tried
-        echo "Trying " . $artistNames[$i] . "... <br>";
+        // look for the birth place or origin of the artist on dbpedia
+        for ($i = 0; $i <= (count($artistNames)-1); $i++) {
+          $query = "
+          PREFIX property: <http://dbpedia.org/property/>
+          PREFIX dbpedia: <http://dbpedia.org/resource/>
+          PREFIX ontology: <http://dbpedia.org/ontology/>
 
-        $result = sparql_query($query);
+          SELECT DISTINCT ?birth_place ?band_origin
+          WHERE {
+            OPTIONAL {<http://dbpedia.org/resource/".$artistNames[$i]."> ontology:birthPlace ?birth_place} .
+            OPTIONAL {<http://dbpedia.org/resource/".$artistNames[$i]."> property:origin ?band_origin} .
+          }
+          LIMIT 5";
 
-        if (is_object($result)) {
-          if (!empty($result->rows[0])) {
-            echo "Result: ";
-            foreach($result->rows as $row) {
+          // print which artist names have been tried
+          echo "Trying " . $artistNames[$i] . "... <br>";
 
-              if (array_key_exists("band_origin", $row)){
-                // remove any urls
-                $band_origin = str_replace("http://dbpedia.org/resource/", "", $row["band_origin"]["value"]);
-                $band_origin = str_replace("_", " ", $band_origin);
-                echo $band_origin . " ";
+          $result = sparql_query($query);
+
+          // If there is a result
+          if (is_object($result)) {
+            // and the first row is not empty
+            if (!empty($result->rows[0])) {
+              echo "Result: ";
+
+              // Loop through each row
+              foreach($result->rows as $row) {
+
+                // make sure either the band origin or birth place has a value
+                if (array_key_exists("band_origin", $row) || array_key_exists("birth_place", $row)) {
+                  if (array_key_exists("band_origin", $row)){
+                    // remove any urls
+                    $origin = $origin . " " . str_replace("http://dbpedia.org/resource/", "", $row["band_origin"]["value"]);
+                  }
+                  else if (array_key_exists("birth_place", $row)){
+                    $origin = $origin . " " . str_replace("http://dbpedia.org/resource/", "", $row["birth_place"]["value"]);
+                  }
+
+                  // clean up the origin name
+                  $origin = str_replace("_", " ", $origin);
+                  // remove two white spaces in a row
+                  $origin = preg_replace('!\s+!', ' ', $origin);
+                  $origin = utf8_decode(urldecode($origin));
+
+                  echo $origin . " ";
+                }
               }
-
-              else if (array_key_exists("birth_place", $row)){
-                $birth_place = str_replace("http://dbpedia.org/resource/", "", $row["birth_place"]["value"]);
-                $birth_place = str_replace("_", " ", $birth_place);
-                echo $birth_place . " ";
-              }
-
-              // // Prepared statement
-              // if (!($stmt = $db->prepare("INSERT INTO `artist_locations` (`artist`, `origin`, `latitude`, `longitude`)
-              //     VALUES ((SELECT `id` FROM `members` WHERE email=\"".$db->real_escape_string($_SESSION['valid_member'])."\"), ?, ?, NOW())"))) {
-              //     echo "Prepare failed: (" . $db->errno . ") " . $db->error;
-              // }
+              // if a query for the artist's origin is successful, stop looping
+              break;
             }
-            // once a query is successful, stop looping
-            break;
           }
         }
+        // insert into the database
+        $res = $mysqli->query("INSERT INTO `artist_locations`(`artist`, `origin`, `latitude`, `longitude`) VALUES (\"".$artist."\",\"".$origin."\",\"\",\"\")");
+        if ($res) {
+          echo "<h2>Successfully inserted to database!</h2>";
+        }
+        else {
+          print_r('Error : ('. $mysqli->errno .') '. $mysqli->error);
+        }
+      }
+      else {
+        echo $artist . " is already in database!";
       }
     }
-
     echo "</pre>";
   }
 }
@@ -143,4 +180,6 @@ if ((($_FILES["file"]["type"] == "application/xml")
 else {
   echo "Invalid file";
 }
+
+
 ?>
