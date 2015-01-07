a8a8cc2a82e3c3a2aa6eaa6316ac5a3dbf4279a5
Danny Blackstock <dannyblackstock@gmail.com>
2015-01-06 22:02:02 -0800

    Add some tests for multiple markers on a Google Map. Add functionality to Geocode locations for artists when they are first added.

--- upload_file.php
+++ upload_file.php
@@ -6,6 +6,56 @@ require_once("sparqllib.php");
 // just in case...
 error_reporting( E_ALL );
 ini_set( "display_errors", "on" );
+?>
+
+<!DOCTYPE html>
+<html> 
+<head> 
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+  <title>Google Maps Multiple Markers</title> 
+  <script src="http://maps.google.com/maps/api/js?sensor=false" 
+          type="text/javascript"></script>
+</head> 
+<body>
+  <div id="map" style="width: 500px; height: 400px;"></div>
+
+  <script type="text/javascript">
+    var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  </script>
+</body>
+</html>
+
+<?php
 
 // database information
 $dbhost = "localhost";
@@ -94,7 +144,11 @@ if ((($_FILES["file"]["type"] == "application/xml")
     foreach($artistList as $artist) {
       echo "<h1>" . $artist . "</h1><br>";
 
+      // reset variables
       $origin = "";
+      $lat = 0;
+      $lng = 0;
+
       $tempQuery = "SELECT * FROM `artist_locations` WHERE `artist` = \"".$artist."\"";
       echo "Query: ".$tempQuery."<br>";
       $res = $mysqli->query($tempQuery);
@@ -103,13 +157,13 @@ if ((($_FILES["file"]["type"] == "application/xml")
       if($res->num_rows < 1) {
         echo $artist . " was not in the database! Adding now...<br>";
 
+        // try a few different combinations of names
         $underscoreArtist = str_replace(" ", "_", $artist);
         $lowerCaseAnd =  str_replace("And", "and", $underscoreArtist);
         $noAmpersandLowercase =  str_replace("&", "and", $underscoreArtist);
         $noAmpersandUppercase =  str_replace("&", "And", $underscoreArtist);
         $upperCaseWords = ucwords(strtolower($underscoreArtist));
-        
-        // try a few different combinations of names
+
         $artistNames = [$underscoreArtist,
                         $upperCaseWords,
                         $underscoreArtist . "_(band)",
@@ -119,7 +173,7 @@ if ((($_FILES["file"]["type"] == "application/xml")
                         $noAmpersandLowercase,
                         $noAmpersandUppercase];
 
-        // look for the birth place or origin of the artist on dbpedia
+        // search for the birth place or origin of the artist on dbpedia
         for ($i = 0; $i <= (count($artistNames)-1); $i++) {
           $query = "
           PREFIX property: <http://dbpedia.org/property/>
@@ -142,10 +196,10 @@ if ((($_FILES["file"]["type"] == "application/xml")
           if (is_object($result)) {
             // and the first row is not empty
             if (!empty($result->rows[0])) {
-              echo "Result: ";
 
               // Loop through each row
               foreach($result->rows as $row) {
+                echo "<b>Result: </b>";
 
                 // make sure either the band origin or birth place has a value
                 if (array_key_exists("band_origin", $row) || array_key_exists("birth_place", $row)) {
@@ -163,28 +217,38 @@ if ((($_FILES["file"]["type"] == "application/xml")
                   $origin = preg_replace('!\s+!', ' ', $origin);
                   $origin = utf8_decode(urldecode($origin));
 
-                  echo $origin . " ";
-                  
-                // $originGeocodeFormat = 
-                // $geocode=file_get_contents("http://maps.google.com/maps/api/geocode/json?address="Victoria+Canada"&sensor=false");
+                  echo $origin . " <br>";
 
-                // $output= json_decode($geocode);
+                  // use google geocode JSON API to get latitude and longitude of the place
+                  $originGeocodeFormat = "http://maps.google.com/maps/api/geocode/json?address=" . str_replace(" ", "+", $origin) . "&sensor=false";
+                  echo "<b>Geocode request: </b>" . $originGeocodeFormat . "<br>";
 
-                // $lat = $output->results[0]->geometry->location->lat;
-                // $lng = $output->results[0]->geometry->location->lng;
+                  $geocode=file_get_contents($originGeocodeFormat);
 
-                // echo $lat . " " . $lng;
+                  $output= json_decode($geocode);
+
+                  $lat = $output->results[0]->geometry->location->lat;
+                  $lng = $output->results[0]->geometry->location->lng;
+
+                  // wait half a second so they google server isn't overloaded
+                  sleep(usleep(500000));
+
+                  echo "<b>Coordinates:</b> " . $lat . " " . $lng . "<br>";
+                  // TODO: add to the javascript array here
                 }
               }
 
               // if a query for the artist's origin is successful, stop looping
               break;
             }
+            else {
+              echo "No location found.<br>";
+            }
           }
         }
 
         // insert into the database
-        $res = $mysqli->query("INSERT INTO `artist_locations`(`artist`, `origin`, `latitude`, `longitude`) VALUES (\"".$artist."\",\"".$origin."\",\"\",\"\")");
+        $res = $mysqli->query("INSERT INTO `artist_locations`(`artist`, `origin`, `latitude`, `longitude`) VALUES (\"".$artist."\",\"".$origin."\",".$lat.",".$lng.")");
         if ($res) {
           echo "<h2>Successfully inserted to database!</h2>";
         }
@@ -193,7 +257,19 @@ if ((($_FILES["file"]["type"] == "application/xml")
         }
       }
       else {
-        echo $artist . " is already in database!";
+        echo $artist . " is already in database! Checking if there's coordinates...<br>";
+        $result = $mysqli->query("SELECT `origin`, `latitude`, `longitude` FROM `artist_locations` WHERE `artist` = \"".$artist."\"");
+        /* associative array */
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+
+        // check if the artist has an origin
+        if (!empty($row["origin"])) {
+          // check if the longitude and latitude are not zero
+          if (($row["latitude"] && $row["longitude"]) == 0) {
+            printf ($row["latitude"] . " " . $row["longitude"]);
+            // TODO: add to the javascript array here
+          }
+        }
       }
     }
     echo "</pre>";
@@ -203,5 +279,4 @@ if ((($_FILES["file"]["type"] == "application/xml")
 else {
   echo "Invalid file";
 }
-
 ?>
